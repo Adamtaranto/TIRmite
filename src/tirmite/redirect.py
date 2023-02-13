2071333b33f2d2694bf4d9fbf31b95e770ab3d60
Adam Taranto <adam.p.taranto@gmail.com>
2023-02-13 16:17:36 +1100

    init redirect module

---
+++ src/tirmite/redirect.py
@@ -0,0 +1,5 @@
+# Check if args[0] is in module dict
+
+# Call module and pass args
+# https://stackoverflow.com/questions/3781851/run-a-python-script-from-another-python-script-passing-in-arguments
+# Check that __name__ updates with redirect

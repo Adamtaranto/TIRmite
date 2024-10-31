a7107a96a075ae0a1219984145e4906f1fa6c524
Adam Taranto <adam.p.taranto@gmail.com>
2024-10-31 16:22:57 +1100

    consolidate cmds to one func

--- src/tirmite/hmmer_wrappers.py
+++ src/tirmite/hmmer_wrappers.py
@@ -8,7 +8,10 @@ from tirmite.wrapping import run_cmd
 
 
 def cleanID(s):
-    """Remove non alphanumeric characters from string. Replace whitespace with underscores."""
+    """
+    Remove non alphanumeric characters from string.
+    Replace whitespace with underscores.
+    """
     s = re.sub(r"[^\w\s]", "", s)
     s = re.sub(r"\s+", "_", s)
     return s
@@ -17,7 +20,9 @@ def cleanID(s):
 def _hmmbuild_command(
     exePath="hmmbuild", modelname=None, cores=None, inAlign=None, outdir=None
 ):
-    """Construct the hmmbuild command"""
+    """
+    Construct the hmmbuild command.
+    """
     # Make model name compliant
     modelname = cleanID(modelname)
     # Check for outdir
@@ -48,7 +53,9 @@ def _hmmbuild_command(
 
 
 def _hmmpress_command(exePath="hmmpress", hmmfile=None):
-    """Construct the hmmbuild command"""
+    """
+    Construct the hmmbuild command.
+    """
     # Base command
     command = quote(exePath) + " -f " + quote(os.path.abspath(hmmfile))
     return command
@@ -64,7 +71,9 @@ def _nhmmer_command(
     cores=None,
     outdir=None,
 ):
-    """Construct the nhmmer command"""
+    """
+    Construct the nhmmer command
+    """
     # Get model hmm basename
     model_base = os.path.splitext(os.path.basename(modelPath))[0]
     # Check for outdir
@@ -96,3 +105,55 @@ def _nhmmer_command(
         + quote(os.path.abspath(genome))
     )
     return command, outdir
+
+
+def cmdScriptHMMER(hmmDir=None, hmmFile=None, alnDir=None, tempDir=None, args=None):
+    if tempDir:
+        tempDir = os.path.abspath(tempDir)
+        if not os.path.isdir(tempDir):
+            os.makedirs(tempDir)
+    else:
+        tempDir = os.getcwd()
+    # Create hmm database dir
+    hmmDB = os.path.join(tempDir, "hmmDB")
+    if not os.path.isdir(hmmDB):
+        os.makedirs(hmmDB)
+    # Copy all existing hmms to hmmDB
+    if hmmDir:
+        for hmm in glob.glob(os.path.join(hmmDir, "*.hmm")):
+            shutil.copy2(hmm, hmmDB)
+    if hmmFile:
+        shutil.copy2(hmmFile, hmmDB)
+    # Write cmds to list
+    cmds = list()
+    # Process alignments
+    if alnDir:
+        build_cmds = list()
+        for alignment in glob.glob(os.path.join(alnDir, "*")):
+            modelName = os.path.splitext(os.path.basename(alignment))[0]
+            hmmbuildCmd, modelPath = _hmmbuild_command(
+                exePath=args.hmmbuild,
+                modelname=modelName,
+                cores=args.cores,
+                inAlign=alignment,
+                outdir=tempDir,
+            )
+            build_cmds.append(hmmbuildCmd)
+        run_cmd(build_cmds, verbose=args.verbose, keeptemp=args.keeptemp)
+    # Press and write nhmmer cmd for all models in hmmDB directory
+    for hmm in glob.glob(os.path.join(hmmDB, "*.hmm")):
+        hmmPressCmd = _hmmpress_command(exePath=args.hmmpress, hmmfile=hmm)
+        nhmmerCmd, resultDir = _nhmmer_command(
+            exePath=args.nhmmer,
+            nobias=args.nobias,
+            matrix=args.matrix,
+            modelPath=hmm,
+            genome=args.genome,
+            evalue=args.maxeval,
+            cores=args.cores,
+            outdir=tempDir,
+        )
+        cmds.append(hmmPressCmd)
+        cmds.append(nhmmerCmd)
+    # Return list of cmds and location of file result files
+    return cmds, resultDir, hmmDB

02c0f72d981c4f0df114535db2561b4c4c55b253
Adam Taranto <adam.p.taranto@gmail.com>
2025-07-17 17:51:04 +1000

    format

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,12 @@
-import pytest
 from tirmite.utils import cleanID, getTimestring
+
 
 def test_cleanID():
     """Test the cleanID function properly cleans strings."""
-    assert cleanID("test-string!") == "teststring"
-    assert cleanID("test string") == "test_string"
-    assert cleanID("test_string") == "test_string"
+    assert cleanID('test-string!') == 'teststring'
+    assert cleanID('test string') == 'test_string'
+    assert cleanID('test_string') == 'test_string'
+
 
 def test_getTimestring():
     """Test that getTimestring returns a string."""
